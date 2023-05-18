76c1d6dbec6e77bcab95a2296d7261f4c5d5cdc2
github-gauravkaushik <gaurav.k.k@gmail.com>
2023-05-18 10:15:35 +0530

    update src/train_and_evaluate.py

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -74,10 +74,6 @@ def train_and_evaluate(config_path):
         json.dump(params,f,indent=4)
 
     ###########################################
-
-
-
-
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir,"model.joblib")
     joblib.dump(lr,model_path)
