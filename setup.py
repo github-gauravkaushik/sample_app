99a63c10c77f0a15d2c957778cb66d8ee9e4d985
github-gauravkaushik <gaurav.k.k@gmail.com>
2023-05-18 12:55:50 +0530

    NOTEBOOK FILE ADDED

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Case study project for MLOps",
+    author="GauravKaushik",
+    packages=find_packages(),
+    license="MIT"
+)
